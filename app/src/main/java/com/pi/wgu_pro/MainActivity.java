56eb75a9ec9dc85e0974a8e0c8ee63368c6f83d2
Payam <payam.farah@live.com>
2021-04-20 09:38:48 -0700

    added second activity for term list

--- app/src/main/java/com/pi/wgu_pro/MainActivity.java
+++ app/src/main/java/com/pi/wgu_pro/MainActivity.java
@@ -1,14 +1,41 @@
 package com.pi.wgu_pro;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button enterBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        enterBtn = (Button) findViewById(R.id.enterBtn);
+
+/**
+ * Setting up event handler for button click to switch activity.
+ */
+       enterBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                launchActivity();
+            }
+        });
+
     }
+
+
+    private void launchActivity() {
+
+        Intent intent = new Intent(this, TermList.class);
+        startActivity(intent);
+    }
+
 }
