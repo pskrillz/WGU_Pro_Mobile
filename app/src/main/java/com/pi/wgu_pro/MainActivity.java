35a91fdd08f3941e3f608e5a5340d3259c67fe8c
Payam <payam.farah@live.com>
2021-04-20 09:06:47 -0700

    first

---
+++ app/src/main/java/com/pi/wgu_pro/MainActivity.java
@@ -0,0 +1,14 @@
+package com.pi.wgu_pro;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
